88131e9059dd261ea6f8b23986dffbc9157a02da
Joao Osorio <joao.l.osorio@gmail.com>
2023-07-31 20:25:54 +0100

    Cleaning project.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,11 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import React from 'react'
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
+const HomePage = () => {
   return (
-    <>
-      <h1>Hello Next World!</h1>
-    </>
-  );
+    <div>
+      <h1>The Home Page</h1>
+    </div>
+  )
 }
+
+export default HomePage
