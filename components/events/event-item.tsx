7ae3b39cabf43b8815a74eb387426a5abf23cff4
Joao Osorio <joao.l.osorio@gmail.com>
2023-08-01 20:35:53 +0100

    Created demo project. Added event list and event item components. Added static files (dummy-data and images).

---
+++ components/events/event-item.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+import React from 'react';
+import styles from './event-item.module.css';
+
+const EventItem = ({
+  title,
+  image,
+  date,
+  location,
+  id,
+}: {
+  title: string;
+  image: string;
+  date: string;
+  location: string;
+  id: string;
+}) => {
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+  const formattedAddress = location.replace(',', '\n');
+  const exploreLink = `/events/${id}`;
+
+  return (
+    <li className={styles.item}>
+      <img src={'/' + image} alt={title} />
+      <div className={styles.content}>
+        <div className={styles.summary}>
+          <h2>{title}</h2>
+          <div className={styles.date}>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={styles.address}>
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div className={styles.actions}>
+          <Link href={exploreLink}>Explore Event</Link>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default EventItem;
