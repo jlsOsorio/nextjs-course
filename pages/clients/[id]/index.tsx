10026f02a85f9e4a1bebd588ea7b2966a29627b2
Joao Osorio <joao.l.osorio@gmail.com>
2023-08-01 16:03:28 +0100

    Added example of a programmatic navigation.

--- pages/clients/[id]/index.tsx
+++ pages/clients/[id]/index.tsx
@@ -5,9 +5,22 @@ const ClientProjectsPage = () => {
   const router = useRouter();
 
   console.log(router.query);
+
+  function loadProjectHandler(): void {
+    // load data...
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: {
+        id: 'joao',
+        clientprojectid: 'projecta',
+      },
+    });
+  }
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
