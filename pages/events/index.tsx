682d9afbc743021f6597b5f2805df806b1e6e521
Joao Osorio <joao.l.osorio@gmail.com>
2023-08-07 09:58:48 +0100

    Created all events page.

---
+++ pages/events/index.tsx
@@ -0,0 +1,15 @@
+import EventList from '@/components/events/event-list';
+import { getAllEvents } from '@/dummy-data';
+import React from 'react';
+
+const EventsPage = () => {
+  const events = getAllEvents();
+
+  return (
+    <div>
+      <EventList items={events} />
+    </div>
+  );
+};
+
+export default EventsPage;
