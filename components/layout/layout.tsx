606c27199977117f628b3fdc632805afba53c75d
Joao Osorio <joao.l.osorio@gmail.com>
2023-08-07 09:56:58 +0100

    Added and wrapped navigation generally.

---
+++ components/layout/layout.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react';
+import MainHeader from './main-header';
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <>
+      <MainHeader />
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
