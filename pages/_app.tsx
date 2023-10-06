31384b1ed518b43d5a0c20a163223516c45f7108
Joao Osorio <joao.l.osorio@gmail.com>
2023-10-06 18:36:38 +0100

    Created and explained how NextJS works with multiple heads in same component, in order to render correctly into the pages.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Head>
+        <title>Next Events</title>
+        <meta name="description" content="NextJS Events" />{' '}
+        {/* O NextJS faz merge dos vários heads e, em caso de conflito, aplica o último Head (em forma de cascata) */}
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
