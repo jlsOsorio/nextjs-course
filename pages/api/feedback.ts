8c5fee391ba10967935e9c1802d418e48b5b79f7
Joao Osorio <joao.l.osorio@gmail.com>
2023-10-08 00:19:35 +0100

    Added api route page and created a handler function with a response example.

---
+++ pages/api/feedback.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({
+    message: 'This works!',
+  });
+}
+
+export default handler;
