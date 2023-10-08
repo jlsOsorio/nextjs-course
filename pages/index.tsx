4266a3629298e41a6f670e00724b81710aece1d8
Joao Osorio <joao.l.osorio@gmail.com>
2023-10-08 01:10:41 +0100

    Created form to talk with api route.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { FormEvent } from 'react';
 
 const HomePage = () => {
+  const emailInputRef = React.useRef<HTMLInputElement>(null);
+  const feedbackInputRef = React.useRef<HTMLTextAreaElement>(null);
+
+  function submitFormHandler(event: FormEvent) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current?.value;
+    const enteredFeedback = feedbackInputRef.current?.value;
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows={5} ref={feedbackInputRef} />
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
